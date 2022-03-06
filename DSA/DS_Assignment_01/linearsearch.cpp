d1a8baf7c549e43e8fc3b8d47597f9c1ae875d33
Rohit Kumar <67726628+rohitverse@users.noreply.github.com>
2022-03-06 11:28:57 +0530

    Add files via upload

---
+++ DSA/DS_Assignment_01/linearsearch.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+bool search(int arr[],int size,int key)
+{
+    for(int i=0;i<size;i++)
+    {
+    if(arr[i]==key)
+    {
+        return 1;
+    }
+    }
+    return 0;
+}
+int main()
+{
+    int arr[10]={5,7,3,4,12,-23,-9,-88,77,3};
+    cout <<"Enter the key"<<endl;
+    int key;
+    cin>>key;
+bool found=search(arr,10,key);
+{
+    if(found)
+    {
+        cout<<"Key is Present"<<endl;
+    }
+    else
+    cout<<"key is not present"<<endl;
+}
+return 0;
+}
